e9c044df47b7edda90108255ec767e4d78fede13
Yuns <yuns8708@gmail.com>
2025-01-06 22:37:06 +0900

    예외처리

---
+++ src/main/java/com/dayone/exception/impl/AlreadyExistUserException.java
@@ -0,0 +1,16 @@
+package com.dayone.exception.impl;
+
+import com.dayone.exception.AbstractException;
+import org.springframework.http.HttpStatus;
+
+public class AlreadyExistUserException extends AbstractException {
+    @Override
+    public int getStatusCode() {
+        return HttpStatus.BAD_REQUEST.value();
+    }
+
+    @Override
+    public String getMessage() {
+        return "이미 존재하는 사용자명입니다.";
+    }
+}
--- src/main/java/com/dayone/service/MemberService.java
+++ src/main/java/com/dayone/service/MemberService.java
@@ -1,5 +1,6 @@
 package com.dayone.service;
 
+import com.dayone.exception.impl.AlreadyExistUserException;
 import com.dayone.model.MemberEntity;
 import com.dayone.model.constants.Auth;
 import com.dayone.persist.MemberRepository;
@@ -28,7 +29,7 @@ public class MemberService implements UserDetailsService {
     public MemberEntity register(Auth.SignUp member) {
         boolean exists = this.memberRepository.existsByUsername(member.getUsername());
         if (exists) {
-            throw new RuntimeException("이미 사용 중인 아이디입니다.");
+            throw new AlreadyExistUserException();
         }
 
         member.setPassword(this.passwordEncoder.encode(member.getPassword()));
